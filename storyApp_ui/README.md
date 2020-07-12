3b866f21a932aab111c4f779f0c8785000d202e1
Bhargav Sridhar <ramsbhargav@gmail.com>
2020-07-12 23:02:46 +0530

    Initial commit

---
+++ storyApp_ui/README.md
@@ -0,0 +1,5 @@
+Story App
+
+Cards horizontal scroll
+
+https://youtu.be/5KbiU-93-yU
---
+++ storyApp_ui/lib/data.dart
@@ -0,0 +1,14 @@
+  
+List<String> images = [
+  "assets/one.jpg",
+  "assets/two.jpg",
+  "assets/three.jpg",
+  "assets/four.jpg",
+];
+
+List<String> title = [
+  "Hounted Ground",
+  "Fallen In Love",
+  "The Dreaming Moon",
+  "Jack the Persian and the Black Castel",
+];
